d750fb2e293fd23170b9b1366f6f2f49437bdf6c
yuta-eng-prg <horii6773@gmail.com>
2020-07-30 13:38:55 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "messages#index"
   resources :users, only: [:edit, :update]
+
+  get "/rooms/new", to: "rooms#new" 
+  resources :rooms, only: [:new, :create]
+
 end
